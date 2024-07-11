153305f4a4016bc70235264a809d74ab2dc83cc5
Rolando Caceres <ROLYCACERES@GMAIL.COM>
2024-07-11 11:11:48 -0400

    complete the Rock, Paper, Scissors game

--- script.js
+++ script.js
@@ -17,4 +17,90 @@ function getComputerChoice()
     return computerChoice;
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    let humanChoice = prompt("Please select Rock, Paper, or Scissor: ");
+    return humanChoice;
+}
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+
+function playRound(humanChoice, computerChoice)
+{
+    //Checks if choices are equal
+    if(humanChoice.toUpperCase() === computerChoice.toUpperCase())
+    {
+        return "It's a tie!"
+    }
+
+
+    else if (humanChoice.toUpperCase() === "ROCK" && computerChoice.toUpperCase() === "PAPER")
+    {
+        computerScore++;
+        return "You lose! Paper beats Rock";
+    }
+    else if (humanChoice.toUpperCase() === "ROCK" && computerChoice.toUpperCase() === "SCISSORS")
+    {
+        humanScore++;
+        return "You win! Rock beats Scissors";
+    }
+
+
+    else if (humanChoice.toUpperCase() === "PAPER" && computerChoice.toUpperCase() === "ROCK")
+    {
+        humanScore++;
+        return "You win! Paper beats Rock";
+    }
+    else if (humanChoice.toUpperCase() === "PAPER" && computerChoice.toUpperCase() === "SCISSORS")
+    {
+        computerScore++;
+        return "You lose! Scissors beats Paper";
+    }
+
+
+    else if (humanChoice.toUpperCase() === "SCISSORS" && computerChoice.toUpperCase() === "PAPER")
+    {
+        humanScore++;
+        return "You win! Scissors beats Paper";
+    }
+    else if (humanChoice.toUpperCase() === "SCISSORS" && computerChoice.toUpperCase() === "ROCK")
+    {
+        computerScore++;
+        return "You lose! Rock beats Scissors";
+    }
+}
+
+function finalWinner()
+{
+    if(humanScore > computerScore)
+    {
+        console.log(`Human wins with ${humanScore} points`);
+    }
+    else if(computerScore > humanScore)
+    {
+        console.log(`Computer wins with ${computerScore} points`);
+    }
+    else
+    {
+        console.log("It's a tie!");
+    }
+}
+
+
+function playGame()
+{
+    for(let i = 0; i < 5; i++)
+        {
+            const humanSelection = getHumanChoice();
+            const computerSelection = getComputerChoice();
+            const result = playRound(humanSelection, computerSelection);
+            console.log(result);
+            console.log(`Human Score: ${humanScore}, Computer Score: ${computerScore}`);
+        }
+        finalWinner();
+}
+
+playGame();
